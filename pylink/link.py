f368ad3936d78b7101f9fcd61a8ffa65c893cbcf
Igor Brkic <igor@hyperglitch.com>
2020-03-20 00:57:15 +0100

    set socket to nonblocking before setting the timeout
    setting the timeout on a socket should be equivalent to setting it as
    nonblocking. however, sometimes i get really long "hang" on connect()
    without timing out so this is an attempt to fix that.

--- pylink/link.py
+++ pylink/link.py
@@ -87,9 +87,9 @@ class TCPLink(Link):
         '''Open the socket.'''
         if self._socket is None:
             self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._socket.setblocking(0)
             self._socket.settimeout(self.timeout)
             self._socket.connect(self.address)
-            self.socket.setblocking(0)
             LOGGER.info('new %s was initialized' % self)
 
     def settimeout(self, timeout):
